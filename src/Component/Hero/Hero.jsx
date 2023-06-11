4dbc61cc5d374ae12d10fcd53f1ad4d62decaa73
Bappy71 <19103076@iubat.edu>
2023-06-12 04:08:55 +0600

    first commit

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
 import "./Hero.css";
 
 const Hero = () => {
@@ -15,13 +16,40 @@ const Hero = () => {
             </h1>
           </div>
           <div className="hero-discription flexColStart">
-            <span>Find a variety of properties that suit you very easilty</span>
-            <span>Forget all difficulties in finding a residence for you</span>
+            <span className="secondaryText">
+              Find a variety of properties that suit you very easilty
+            </span>
+            <span className="secondaryText">
+              Forget all difficulties in finding a residence for you
+            </span>
           </div>
           <div className="search-bar flexCenter">
             <HiLocationMarker color="var(--blue)" size={25} />
             <input type="text" />
             <button className="button">search</button>
+          </div>
+          <div className="stats flexCenter">
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={88000} end={9000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">PREMIUM PRODUCT</span>
+            </div>
+            <div className="flexColStart stat">
+              <span>
+                <CountUp start={1950} end={2000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">HAPPY CUSTOMERS</span>
+            </div>
+            <div className="flexColStart stat">
+              <span>
+                <CountUp end={28} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">AWARD WINNINGS</span>
+            </div>
           </div>
         </div>
         {/* HERO RIGHT SECTIONS */}
