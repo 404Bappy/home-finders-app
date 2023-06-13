82d03c76e6195cd22b98c68faf3fdd59630d7dd9
Bappy71 <19103076@iubat.edu>
2023-06-14 00:03:30 +0600

    first commit

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 import "./Contact.css";
+import { MdCall } from "react-icons/md";
+import { BsFillChatDotsFill } from "react-icons/bs";
+import { HiChatBubbleBottomCenter } from "react-icons/hi";
 
 const Contact = () => {
   return (
     <section className="contact-wrapper">
       <div className="paddings innerWidth flexCenter Contact-Container">
         {/* LEFT SIDE OF CONTACT  */}
-        <div className="c-left">left</div>
+        <div className="c-left flexColStart">
+          <sapn className="orangeText">Our Contact</sapn>
+          <sapn className="primaryText">Easy To Contact Us</sapn>
+          <sapn className="secondaryText">
+            We always ready to help by providijng the best services for you. We
+            beleive a good blace to live can make your life better
+          </sapn>
+
+          <div className="flexColStart contactModes">
+            {/* FIRST ROW  */}
+            <div className="flexColStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <MdCall size={25} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
         {/* RIGHT SIDE OF CONTACT  */}
         <div className="c-right">
           <div className="image-container">
-            <img src="./contct.jpg" alt="" />
+            <img src="./contact.jpg" alt="" />
           </div>
         </div>
       </div>
